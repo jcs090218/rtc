0335996d137a30377c26d3dc6b9878a981f1b69c
jenchieh <jcs090218@gmail.com>
2017-12-11 16:37:35 +0800

    add rtc master load.

---
+++ rtc.h
@@ -0,0 +1,30 @@
+#ifndef __RTC_H__
+/**
+ * $File: rtc.h $
+ * $Date: 2017-12-03 12:08:55 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2017 by Shen, Jen-Chieh $
+ */
+#define __RTC_H__
+
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdbool.h>
+
+/* Libraries */
+#include "./include/libc-jcs/libc_jcs.h"
+
+/* Defines */
+#include "./include/defines.h"
+#include "./include/util.h"
+
+#endif /* __RTC_H__ */
