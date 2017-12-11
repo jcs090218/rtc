b245bb14e77464d1939bfd20e17f8ee310de21f5
jenchieh <jcs090218@gmail.com>
2017-12-12 03:18:45 +0800

    add ope file.

---
+++ include/ope.h
@@ -0,0 +1,37 @@
+#ifndef __OPE_H__
+/**
+ * $File: ope.h $
+ * $Date: 2017-12-12 03:03:40 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2017 by Shen, Jen-Chieh $
+ */
+#define __OPE_H__
+
+
+/**
+ * @func rtc_init
+ * @brief Initialize the RTC application.
+ */
+void rtc_init(void);
+
+/**
+ * @func rtc_print_inc
+ * @brief Print command instructions.
+ */
+void rtc_print_inc(void);
+
+/**
+ * @func rtc_print_ver
+ * @brief Function description here..
+ */
+void rtc_print_ver(void);
+
+/**
+ * @func rtc_status
+ * @brief RTC status command.
+ */
+void rtc_status(void);
+
+#endif /* __OPE_H__ */
