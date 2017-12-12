f13f67db1b9345dcdaa8902899ff1e4b7c68a08b
jenchieh <jcs090218@gmail.com>
2017-12-12 17:25:15 +0800

    add command list.

---
+++ include/commands.h
@@ -0,0 +1,26 @@
+#ifndef __COMMANDS_H__
+/**
+ * $File: commands.h $
+ * $Date: 2017-12-12 15:40:48 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2017 by Shen, Jen-Chieh $
+ */
+#define __COMMANDS_H__
+
+/*
+ * List of all commands.
+ */
+
+#define RTC_CMD_INIT          "init"
+#define RTC_CMD_VERSION_CHECK "--version"
+#define RTC_CMD_HELP          "--help"
+#define RTC_CMD_STATUS        "status"
+
+#define RTC_CMD_START_SERVER "start_server"
+#define RTC_CMD_CLOSE_SERVER "close_server"
+#define RTC_CMD_CONNECT      "connect"
+#define RTC_CMD_DISCONNECT   "disconnect"
+
+#endif /* __COMMANDS_H__ */
